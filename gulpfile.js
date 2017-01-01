50c15cd1024e0aa77b44a7f1fd2f89679fb00f8e
Manraj Singh <manrajsinghgrover@gmail.com>
2017-01-01 23:04:21 +0530

    Fix: Linting errors in Gulpfile fixed

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 const gulp = require('gulp');
-const browserify = require("browserify");
-const babelify = require("babelify");
+const browserify = require('browserify');
+const babelify = require('babelify');
 const source = require('vinyl-source-stream');
 const gutil = require('gulp-util');
 const babel = require('gulp-babel');
@@ -9,9 +9,9 @@ const babel = require('gulp-babel');
 gulp.task('browser', () => {
   browserify({ debug: false })
     .transform(babelify)
-    .require("./sample/example.js", { entry: true })
+    .require('./sample/example.js', { entry: true })
     .bundle()
-    .on('error',gutil.log)
+    .on('error', gutil.log)
     .pipe(source('app.js'))
     .pipe(gulp.dest('./sample/source'));
 });
@@ -19,13 +19,13 @@ gulp.task('browser', () => {
 gulp.task('es6', () => {
   gulp.src('src/*.js')
     .pipe(babel({
-      presets: ['es2015']
+      presets: ['es2015'],
     }))
-    .pipe(gulp.dest('./dist'))
+    .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('watch', () => {
-  gulp.watch(['./src/*.js'],['browser', 'es6'])
+  gulp.watch(['./src/*.js'], ['browser', 'es6']);
 });
- 
+
 gulp.task('default', ['browser', 'es6']);
