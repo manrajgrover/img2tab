3e1e7190f3f32a919bbdb00c0a5cbad669df4a8a
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-02-24 23:07:36 +0530

    Update: Image converted to table

--- src/tablo.js
+++ src/tablo.js
@@ -2,7 +2,7 @@ const jimp = require('jimp');
 
 class Tablo {
 
-  constructor(image) {
+  constructor(image, width = 1, height = 1) {
     if (image === undefined || image === '') {
       throw new Error(
         'Initialization failed. Please check file path passd to constructor.'
@@ -10,6 +10,8 @@ class Tablo {
     }
     this.image = image;
     this.colors = null;
+    this.width = width;
+    this.height = height;
   }
 
   _readImage() {
@@ -46,9 +48,30 @@ class Tablo {
     return pixelCalculation;
   }
 
+  _getImageTable(img) {
+    const pImageTable = new Promise((resolve) => {
+      let imageTable = "<table border='0' cellpadding='0' cellspacing='0'>";
+
+      for (let i = 0; i < img.length; i += 1) {
+        imageTable += '<tr>';
+        for (let j = 0; j < img[i].length; j += 1) {
+          imageTable += `<td height='${this.height}' width='${this.width}' style='background: ${img[i][j]};'>`;
+        }
+        imageTable += '</tr>';
+      }
+
+      imageTable += '</table>';
+      resolve(imageTable);
+    });
+
+    return pImageTable;
+  }
+
   getTablo() {
     return (
-      this._readImage().then(image => this._getPixelColors(image))
+      this._readImage()
+          .then(image => this._getPixelColors(image))
+          .then(pixelColors => this._getImageTable(pixelColors))
     );
   }
 }
