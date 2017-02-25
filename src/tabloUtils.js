e2ac5e9154df5371cdfdc416569d8603b8e830b3
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-02-25 20:54:38 +0530

    Add: TabloUtils class

---
+++ src/tabloUtils.js
@@ -0,0 +1,34 @@
+class TabloUtils {
+
+  static getPixelColors(image) {
+    const width = image.bitmap.width;
+    const height = image.bitmap.height;
+    const pixels = new Array(height);
+
+    for (let i = 0; i < height; i += 1) {
+      pixels[i] = new Array(width);
+    }
+
+    const data = image.bitmap.data;
+
+    const pixelCalculation = new Promise((resolve) => {
+      image.scan(0, 0, width, height, (x, y, idx) => {
+        const red = data[idx + 0];
+        const green = data[idx + 1];
+        const blue = data[idx + 2];
+        const alpha = data[idx + 3] / 255;
+
+        pixels[x][y] = `rgba(${red}, ${green}, ${blue}, ${alpha})`;
+
+        if (width - 1 === x && height - 1 === y) {
+          resolve(pixels);
+        }
+      });
+    });
+
+    return pixelCalculation;
+  }
+
+}
+
+module.exports = TabloUtils;
