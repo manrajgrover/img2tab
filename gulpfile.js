5eef0260b2b4190cead6209f66bff50d93f22973
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-02-25 20:53:13 +0530

    Remove: Browserify code

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,5 @@
 const gulp = require('gulp');
-const browserify = require('browserify');
-const babelify = require('babelify');
-const source = require('vinyl-source-stream');
-const gutil = require('gulp-util');
 const babel = require('gulp-babel');
-
-gulp.task('browser', () => {
-  browserify({ debug: false })
-    .transform(babelify)
-    .require('./sample/example.js', { entry: true })
-    .bundle()
-    .on('error', gutil.log)
-    .pipe(source('app.js'))
-    .pipe(gulp.dest('./sample/source'));
-});
 
 gulp.task('es6', () => {
   gulp.src('src/*.js')
@@ -24,7 +10,7 @@ gulp.task('es6', () => {
 });
 
 gulp.task('watch', () => {
-  gulp.watch(['./src/*.js', './sample/example.js'], ['es6', 'browser']);
+  gulp.watch(['./src/*.js'], ['es6']);
 });
 
-gulp.task('default', ['browser', 'es6']);
+gulp.task('default', ['es6']);
